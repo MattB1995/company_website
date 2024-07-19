8caf25dac4f5d09870aa7cf7ea08fae59eeb6080
Matt Buczkowski <matbuczkowski95@gmail.com>
2024-07-19 21:54:03 +0100

    added back to top button

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-  // The map, centered at Uluru
+  // The map, centered at Leighton Buzzard
   map = new Map(document.getElementById("map"), {
     zoom: 13,
     center: position,
     mapId: "DEMO_MAP_ID",
   });
 
-  // The marker, positioned at Uluru
+  // The marker, positioned at Leighton Buzzard
   const marker = new AdvancedMarkerElement({
     map: map,
     position: position,
@@ -27,3 +27,24 @@ async function initMap() {
 }
 
 initMap();
+
+
+// Get the button:
+let backToTopBtn = document.getElementById("backToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
